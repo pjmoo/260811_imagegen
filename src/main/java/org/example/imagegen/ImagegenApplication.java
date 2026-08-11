a0ec53d0282a246fda46c1d50d1d05f2af07842a
pjmoo <00pjmoooo@gmail.com>
2026-08-11 12:26:25 +0900

    Initial commit

---
+++ src/main/java/org/example/imagegen/ImagegenApplication.java
@@ -0,0 +1,13 @@
+package org.example.imagegen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagegenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImagegenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/imagegen/ImagegenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.imagegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagegenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
